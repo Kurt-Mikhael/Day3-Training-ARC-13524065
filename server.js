ddbecb8211ce7a8970d872526b45b7b69340a73d
Kurt-Mikhael <kurtmikhael123@gmail.com>
2025-03-07 13:34:31 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+const express =  require('express');
+const path = require('path');
+const app = express();
+require('dotenv').config(); // Load environment variables dari .env
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+    });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+    });
+app.get('/script.js', (req, res) => {
+    res.sendFile(path.join(__dirname, 'script.js'));
+    });
+app.get('/style.css', (req, res) =>
+    res.sendFile(path.join(__dirname, 'style.css'))
+    );
+
